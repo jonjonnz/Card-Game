e94a0c7f12e6548574f6803f2e5c2c21a53c5a47
Jagjit Singh Ramgarhia <49023172+jonjonnz@users.noreply.github.com>
2020-07-01 20:13:35 +0530

    wrote code for it and stand using recursion

--- game.py
+++ game.py
@@ -1,6 +1,8 @@
 import deck
 from os import name, system
 from time import sleep
+
+
 # import pygame
 
 
@@ -12,6 +14,89 @@ def clear():
         # for mac and linux(here, os.name is 'posix')
     else:
         _ = system('clear')
+
+
+deck_of_cards = deck.Deck()
+deck_of_cards.shuffle_deck()
+deck_of_cards.shuffle_deck('riffle')
+deck_of_cards.shuffle_deck('cut')
+
+game_on = True
+print('--------------------BlackJack----------------------')
+
+
+# name = input('Enter Name : ')
+# coins = int(input('Enter your Starting value : '))
+def get_total(hand):
+    value = 0
+    for card in hand:
+        if card.value > 9:
+            value += 10
+        elif card.value == 1:
+            if value < 11:
+                value += 11
+            else:
+                value += 1
+        else:
+            value += card.value
+    return value
+
+
+def current_turn(turn, hand):
+    score = get_total(hand)
+    if turn == 'player':
+        if score < 22:
+            choice = input('What do you want to do ? 1.Hit 2.Stand :')
+            if choice == '1':
+                clear()
+                hand.append(deck_of_cards.draw_card('top'))
+                print('You got : ', end='')
+                sleep(1)
+                for card in hand:
+                    print('[{}] '.format(card.show_card()), end='')
+                print('\n')
+                return current_turn('player', hand)
+
+            elif choice == '2':
+                return ['stand', score]
+            else:
+                print('Select Correct input')
+                return current_turn(turn, hand)
+        else:
+            return ['busted', score]
+    elif turn == 'dealer':
+        if score < 17:
+            hand.append(deck_of_cards.draw_card('top'))
+            sleep(1)
+            print('Dealer got : ', end='')
+            for card in hand:
+                print('[{}] '.format(card.show_card()), end='')
+            print('\n')
+            return current_turn(turn,hand)
+        elif score > 21:
+            return ['busted', score]
+        else:
+            return ['stand', score]
+
+
+dealer = []
+player = []
+sequence = [player, dealer, player, dealer]
+busted = None
+while game_on:
+    clear()
+    for seq in sequence:
+        seq.append(deck_of_cards.draw_card('top'))
+    bet = int(input('Enter bet : '))
+    print('Dealer got : [{}] [?]'.format(dealer[0].show_card()))
+    sleep(1)
+    print('You got : [{}] [{}]'.format(player[0].show_card(), player[1].show_card()))
+    player_busted, player_score = current_turn('player', player)
+    if player_busted != 'busted':
+        print('Dealer got : [{}] [{}]'.format(dealer[0].show_card(),dealer[1].show_card()))
+        dealer_busted, dealer_score = current_turn('dealer',dealer)
+
+    game_on = False
 
 # # Predefined Values
 
@@ -40,95 +125,3 @@ def clear():
 #                 screen.blit(card.image, (0, 0))
 #     pygame.display.update()
 # pygame.quit()
-
-deck_of_cards = deck.Deck()
-deck_of_cards.shuffle_deck()
-deck_of_cards.shuffle_deck('riffle')
-deck_of_cards.shuffle_deck('cut')
-
-game_on = True
-print('--------------------BlackJack----------------------')
-
-
-# name = input('Enter Name : ')
-# coins = int(input('Enter your Starting value : '))
-def get_total(hand):
-    value = 0
-    for card in hand:
-        if card.value > 9:
-            value += 10
-        elif card.value == 1:
-            if value < 11:
-                value += 11
-            else:
-                value += 1
-        else:
-            value += card.value
-    return value
-
-def current_turn(turn, hand):
-    if turn == 'player':
-        if get_total(hand) < 22:
-            choice = input('What do you want to do ? 1.Hit 2.Stand :')
-            if choice == '1':
-                clear()
-                player.append(deck_of_cards.draw_card('top'))
-                print('You got : ', end='')
-                sleep(1)
-                for card in player:
-                    print('[{}] '.format(card.show_card()), end='')
-                print('\n')
-                current_turn('player', hand)
-
-            elif choice == '2':
-                return 'stand'
-            else:
-                print('Select Correct input')
-                current_turn(turn,hand)
-        else:
-            return 'busted'
-
-comp = []
-player = []
-sequence = [player, comp, player, comp]
-busted = None
-while game_on:
-    clear()
-    for turn in sequence:
-        turn.append(deck_of_cards.draw_card('top'))
-    bet = int(input('Enter bet : '))
-    print('Computer got : [{}] [?]'.format(comp[0].show_card()))
-    sleep(1)
-    print('You got : [{}] [{}]'.format(player[0].show_card(), player[1].show_card()))
-    player_busted = current_turn('player',player)
-    if player_busted != 'busted':
-        player_score = get_total(player)
-
-    # add_card = True
-    # while add_card:
-    #     choice = input('What do you want to do ? 1.Hit 2.Stand :')
-    #     if choice == 'h':
-    #         player.append(deck_of_cards.draw_card('top'))
-    #         print('You got : ', end='')
-    #         for card in player:
-    #             print(card.show_card() + ' ', end='')
-    #         print('\n')
-    #     elif choice == 's':
-    #         add_card = False
-    #     if get_total(player) > 21:
-    #         add_card = False
-    #         busted = 'p'
-    # if not busted:
-    #     print('Computer got {} {} : '.format(comp[0].show_card(), comp[1].show_card()))
-    #     while get_total(comp) < 17:
-    #         comp.append(deck_of_cards.draw_card)
-    #         print('Computer got : ', end='')
-    #         for card in comp:
-    #             print(card.show_card() + ' ', end='')
-    #         print('\n')
-    #     if get_total(comp) > 21:
-    #         busted = 'c'
-
-    game_on = False
-
-
